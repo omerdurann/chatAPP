87975f47e861290e5f70271c880b42b24fa3c105
omerdurann <omerduran4433@gmail.com>
2022-10-31 03:29:38 +0300

    v

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase' ;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+  };
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase
