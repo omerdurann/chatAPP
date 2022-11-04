a44397c9e6829c958b23643e5c1d60603569cf2a
omerdurann <omerduran4433@gmail.com>
2022-11-04 21:20:35 +0300

    v0.2

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZlbzFdmTW0Nq06ztnL08x__Fi5O04bZg",
-  authDomain: "chatapp-f0410.firebaseapp.com",
-  databaseURL: "https://chatapp-f0410-default-rtdb.firebaseio.com",
-  projectId: "chatapp-f0410",
-  storageBucket: "chatapp-f0410.appspot.com",
-  messagingSenderId: "916135653260",
-  appId: "1:916135653260:web:fe5a734f60960a655a30a9",
-  measurementId: "G-WQVQHP4W5B"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 // Initialize Firebase
